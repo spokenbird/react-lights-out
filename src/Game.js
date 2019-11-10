ae8fd037d76bb6f7830c892a6965349b0ef0a4fd
jyahn <jooyoungxahn@gmail.com>
2019-11-09 21:15:37 -0800

    fixed logic that successfully renders game upon user selecting difficulty mode

--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,33 @@ import React, { Component } from 'react'
 import Board from "./Board"
 
 class Game extends Component {
-  render(){
-    return(
-        <Board/>
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: ""
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(evt, mode) {
+    evt.preventDefault();
+    this.setState({
+      mode
+    })
+  }
+
+
+  render() {
+    console.log("state in game", this.state)
+    return (<div >
+      <h1> Choose your difficulty: </h1>
+      <button onClick={(e) => { this.handleClick(e, 'easy') }}>Easy</button>
+      <button onClick={(e) => { this.handleClick(e, 'medium') }}>Medium</button>
+      <button onClick={(e) => { this.handleClick(e, 'hard') }}>Hard</button>
+      {this.state.mode === 'easy' ? < Board nrows={4} ncols={4} numClicks={4} /> : null}
+      {this.state.mode === 'medium' ? < Board nrows={5} ncols={5} numClicks={5} /> : null}
+      {this.state.mode === 'hard' ? < Board nrows={5} ncols={5} numClicks={7} /> : null}
+    </div >
     )
   }
 }
