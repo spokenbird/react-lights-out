f4a36ffa4fc0b9ffa6e1a60a941efaded8b59e52
spokenbird <39173486+spokenbird@users.noreply.github.com>
2019-11-12 13:02:28 -0800

    Updates

---
+++ precache-manifest.99e29fddf9c3689f8d68747075a52110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b64602b17f4a02281d8",
+    "url": "/react-lights-out/static/css/main.f9678627.chunk.css"
+  },
+  {
+    "revision": "4b64602b17f4a02281d8",
+    "url": "/react-lights-out/static/js/main.4b64602b.chunk.js"
+  },
+  {
+    "revision": "02d3ed819a99a39d2fed",
+    "url": "/react-lights-out/static/js/1.02d3ed81.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "0bd9873f1e416ef2f7af58000f14dfc5",
+    "url": "/react-lights-out/index.html"
+  }
+];
